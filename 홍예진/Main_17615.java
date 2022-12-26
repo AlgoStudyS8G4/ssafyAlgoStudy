1f4f052febacdf6e85dd8e0a8ada6186988d7725
YeJinHong <teepij@naver.com>
2022-12-26 23:59:04 +0900

    solve Main 17615

--- 홍예진/Main_17615.java
+++ 홍예진/Main_17615.java
@@ -10,13 +10,13 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		char[] chrArr = br.readLine().toCharArray();
 		int red = 0, blue = 0;
-		for(char chr : chrArr) {
-			if(chr == 'R')
+		for (char chr : chrArr) {
+			if (chr == 'R')
 				red++;
 			else
 				blue++;
 		}
-		
+
 		char left = chrArr[0];
 		int leftCount = 0;
 
@@ -33,17 +33,32 @@ public class Main {
 				break;
 			rightCount++;
 		}
-		// 작성중
+
 		int ans = Integer.MAX_VALUE;
 		// 1. 볼을 왼쪽으로 옮길 때
-		if(left == 'R') {
-			// 빨간 공을 옮긴다.
+		if (left == 'R') {
+			// 왼쪽 끝에 빨강공이 있고, 빨간 공을 옮긴다.
 			ans = Math.min(ans, red - leftCount);
+			// 왼쪽 끝에 빨간 공이 있고, 파란 공을 옮긴다.
+			ans = Math.min(ans, blue);
 		} else {
+			// 왼쪽 끝에 파란공이 있고, 파란공을 옮긴다.
 			ans = Math.min(ans, blue - leftCount);
+			// 왼쪽 끝에 파란 공이 있고, 빨간 공을 옮긴다.
+			ans = Math.min(ans, red);
+		}
+
+		// 2. 볼을 오른쪽으로 옮길 때
+		if (right == 'R') {
+			ans = Math.min(ans, red - rightCount);
+			ans = Math.min(ans, blue);
+		} else {
+			ans = Math.min(ans, blue - rightCount);
+			ans = Math.min(ans, red);
 		}
 		
 		
+		System.out.println(ans);
 
 	}
 }
