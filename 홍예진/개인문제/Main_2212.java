22b703562f77191060e5edf87abdbb32752d45c9
YeJinHong <teepij@naver.com>
2023-01-03 21:39:08 +0900

    solve Main_2212

--- 홍예진/개인문제/Main_2212.java
+++ 홍예진/개인문제/Main_2212.java
@@ -1,30 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
-	static int N, K;
-	static int[] point;
 
-	public static void solve() {
-		// 각 집중국은 센서의 수신 가능 영역을 조절할 수 있다.
-		// 최대 K개 집중국의 수신 가능 영역 길이 합의 최솟값 출력.
-		// N개의 센서가 적어도 하나의 집중국과는 통신이 가능해야한다.
-		
-		
-	}
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		K = Integer.parseInt(br.readLine());
-		point = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			point[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		solve();
-	}
-
+public class Main{
+    static int N, K;
+    static int[] way_points;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+        way_points = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            way_points[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(way_points);
+        
+        int total = way_points[N-1] - way_points[0];
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int i = 0; i < N-1; i++){
+            int diff = way_points[i+1] - way_points[i];
+            pq.add(diff);
+        }
+        
+        
+        for(int i = 0; !pq.isEmpty() && i < K-1; i++){
+        	total -= pq.poll();
+        }
+        
+        System.out.println(total);
+    }
 }
